7ef0b1aa4af31a4e6cb5cc90aa9950467a04282c
Paula Beatriz <107193691+Paulabeatriz09@users.noreply.github.com>
2022-08-21 20:57:58 -0300

    primeiro commit

---
+++ 1 modulo/1D JS.js
@@ -0,0 +1,9 @@
+
+//https://www.netshoes.com.br/
+//O site utilizado mostra um javascript dentro de um htlm na qual há uma declaração try que marca um bloco de declarações para testar , e especifica uma resposta, caso uma exceção seja lançada.
+
+
+<script type="text/javascript" id="">
+    try{(function(){var c=function(){this.prepare()};c.prototype.prepare=function(){this.checkReffer();this.setRules();this.getVisiteds();this.setPage();this.init()};c.prototype.sendToGA=function(a,b,d,e,f){_ga("send","event",a,b,d,e,{nonInteraction:f})};c.prototype.checkReffer=function(){if(""!=window.document.referrer&&-1==window.
+
+
