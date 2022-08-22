bec5a0605b7e93ea4f52e444fb0881cd6d90be7b
Paula Beatriz <107193691+Paulabeatriz09@users.noreply.github.com>
2022-08-21 21:15:48 -0300

    primeiro commit

--- 1 modulo/1D JS.js
+++ 1 modulo/1D JS.js
@@ -1,5 +1,5 @@
 
-//https://www.netshoes.com.br/
+//netshoes.com.br
 //O site utilizado mostra um javascript dentro de um htlm na qual há uma declaração try que marca um bloco de declarações para testar , e especifica uma resposta, caso uma exceção seja lançada.
 
 
@@ -7,3 +7,17 @@
     try{(function(){var c=function(){this.prepare()};c.prototype.prepare=function(){this.checkReffer();this.setRules();this.getVisiteds();this.setPage();this.init()};c.prototype.sendToGA=function(a,b,d,e,f){_ga("send","event",a,b,d,e,{nonInteraction:f})};c.prototype.checkReffer=function(){if(""!=window.document.referrer&&-1==window.
 
 
+//prolinfo.com.br
+//Função If verifica se a senha está compatível com o Usuário, caso seja falso retornará que está falso e o usuário deve inserir novamente. 
+
+</script>
+    if (senha == 'false') {
+    $('#alert').hide('fast');
+    $('#alert').html('<div class="alert alert-danger"><strong>Usuário ou senha incorretos!</strong>Tente novamente.</div>');
+    $('#alert').show('fast');
+    $('#submit').html('Entrar');
+
+    } else {
+        $("#alert").html(senha);
+    }
+      
