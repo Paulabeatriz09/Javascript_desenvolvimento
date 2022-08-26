869a6bba613d3d9e45bff73d4284ba8d3d874c27
Paula Beatriz <107193691+Paulabeatriz09@users.noreply.github.com>
2022-08-25 23:08:39 -0300

    setimo

---
+++ 3_modulo/2Dfuncion.js
@@ -0,0 +1,22 @@
+// Crie um programa que contenha os seguintes tipos de funções:
+
+
+// 1. uma função tradicional com a palavra reservada “Function” e sem nenhum parâmetro;
+function vazia(){
+
+}
+
+// 2. uma função tradicional com parâmetros e um retorno de valor;
+function soma(a,b){
+    return a+b;
+}
+console.log(soma(2,2));
+
+// 3. uma arrow function com parâmetros e que retorne um valor.
+const som=(a,b)=>a+b;
+
+console.log(com(1,1));
+
+
+
+
